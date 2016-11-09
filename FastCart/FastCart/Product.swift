d32209545ab52da836a1858e469a3a47026d0d57
Luis Perez <luis.perez.live@gmail.com>
2016-11-08 20:53:29 -0800

    gitignore works

---
+++ FastCart/FastCart/Product.swift
@@ -0,0 +1,58 @@
+//
+//  Product.swift
+//  FastCart
+//
+//  Created by Belinda Zeng on 11/7/16.
+//  Copyright © 2016 LemonBunny. All rights reserved.
+//
+
+import UIKit
+
+class Product: NSObject {
+    var productName: NSString?
+        
+    func formatTimeToString(date: NSDate) -> String {
+        let interval = date.timeIntervalSinceNow
+        let intervalInt = Int(interval) * -1
+        let days = (intervalInt / 3600) / 24
+        if days != 0 {
+            let daysStr = String(days) + "d"
+            return daysStr
+        }
+        let hours = (intervalInt / 3600)
+        if hours != 0 {
+            return String(hours) + "h"
+        }
+        
+        let minutes = (intervalInt / 60) % 60
+        if minutes != 0 {
+            return String(minutes) + "m"
+        }
+        
+        let seconds = intervalInt % 60
+        if seconds != 0 {
+            return String(seconds) + "s"
+        }
+        else  {
+            return "Now"
+        }
+    }
+    
+    init(dictionary: NSDictionary) {
+        super.init()
+        productName = dictionary["name"] as? NSString
+        print(productName)
+    }
+    
+    class func productsWithArray(dictionaries: [NSDictionary]) -> [Product]{
+        var products = [Product]()
+        for dictionary in dictionaries {
+            let product =
+                Product.init(dictionary: dictionary)
+            products.append(product)
+        }
+        return products
+    }
+    
+
+}
---
+++ FastCart/FastCart/ScanViewController.swift
@@ -0,0 +1,122 @@
+//
+//  ViewController.swift
+//  FastCart
+//
+//  Created by Belinda Zeng on 11/7/16.
+//  Copyright © 2016 LemonBunny. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import BarcodeScanner
+
+extension ScanViewController: BarcodeScannerCodeDelegate {
+    func barcodeScanner(_ controller: BarcodeScannerController, didCaptureCode code: String, type: String) {
+        print(code)
+        // perform network request
+        print("getting here")
+        WalmartClient.sharedInstance.getProductWithUPC(upc: code, success: { (products: [Product]) in
+            // save product and present correct view
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            // this works
+//            let productDetailsNavigationController = storyboard.instantiateViewController(withIdentifier: "ProductDetailsNavigationController") as! UINavigationController
+//            let productDetailsViewController = storyboard.instantiateViewController(withIdentifier: "ProductDetailsViewController") as! ProductDetailsViewController
+//            productDetailsViewController.product = products[0]
+//            print("product\(products)")
+//            self.present(productDetailsViewController, animated: true, completion: nil)
+//            
+            // try this
+            let productDetailsNavigationController = storyboard.instantiateViewController(withIdentifier: "ProductDetailsNavigationController") as! UINavigationController
+            let productDetailsViewController = productDetailsNavigationController.topViewController as! ProductDetailsViewController
+            productDetailsViewController.product = products[0]
+            print("product\(products)")
+            self.present(productDetailsNavigationController, animated: true, completion: nil)
+            
+//            self.present(productDetailsViewController, animated: true, completion: nil)
+            
+        }, failure: {(error: Error) -> () in
+            print(error.localizedDescription)
+        })
+
+        // dismiss after to test
+        controller.dismiss(animated: true, completion: nil)
+        // controller.reset()
+        
+    }
+}
+
+extension ScanViewController: BarcodeScannerErrorDelegate {
+    
+    func barcodeScanner(_ controller: BarcodeScannerController, didReceiveError error: Error) {
+        print(error)
+    }
+}
+
+extension ScanViewController: BarcodeScannerDismissalDelegate {
+    
+    func barcodeScannerDidDismiss(_ controller: BarcodeScannerController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+class ScanViewController: UIViewController {
+    var product: Product?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("loaded")
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    @IBOutlet weak var scanButton: UIButton!
+    
+    @IBAction func onScanButtonPress(_ sender: Any) {
+        // check to see if camera usage is authorized
+        print("scanning")
+        if AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) ==  AVAuthorizationStatus.authorized
+        {
+            // Already Authorized
+            startScanning()
+        }
+        else
+        {
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { (granted :Bool) -> Void in
+                if granted == true
+                {
+                    // User granted
+                    self.startScanning()
+                }
+                else
+                {
+                    // User Rejected
+                    
+                }
+            });
+        }
+    }
+    
+    func startScanning() {
+        let controller = BarcodeScannerController()
+        controller.codeDelegate = self
+        controller.errorDelegate = self
+        controller.dismissalDelegate = self
+        
+        present(controller, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+
+}
+
