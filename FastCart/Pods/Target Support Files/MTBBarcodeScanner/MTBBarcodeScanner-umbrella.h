ef7b0972a632018085dc71c9aff92578234009e4
Luis Perez <luis.perez.live@gmail.com>
2016-11-08 20:53:02 -0800

    .gitignore is now working

--- FastCart/Pods/Target Support Files/MTBBarcodeScanner/MTBBarcodeScanner-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "MTBBarcodeScanner.h"
-
-FOUNDATION_EXPORT double MTBBarcodeScannerVersionNumber;
-FOUNDATION_EXPORT const unsigned char MTBBarcodeScannerVersionString[];
-
