62574dd2f7cbbd505074ba7507f5c04bec94f79c
Luis Perez <luis.perez.live@gmail.com>
2016-11-15 00:31:17 -0800

    working payment server

--- FastCart/FastCart/PaymentsViewController.swift
+++ FastCart/FastCart/PaymentsViewController.swift
@@ -17,8 +17,6 @@ class PaymentsViewController: UIViewController, BTDropInViewControllerDelegate {
     ///
     /// @param viewController The Drop In view controller informing its delegate of success
     /// @param tokenization The selected (and possibly newly created) tokenized payment information.
-    
-
     var braintreeClient: BTAPIClient?
     let CLIENT_AUTHORIZATION = "sandbox_9tgty665_ys8wr2wffmztcdqn"
     
@@ -35,8 +33,7 @@ class PaymentsViewController: UIViewController, BTDropInViewControllerDelegate {
 //    }
 //    
     func setUpPayments() {
-        let clientToken = "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiI1OTljNzQ5ZDU1Y2NhYWIzNGE5YzM5ZGEzNmJhNDVjZDhhNDk1ZTM1OGNlOTBlOTcyMzU5YzVjNDVjZjdlMjY2fGNyZWF0ZWRfYXQ9MjAxNi0xMS0wOVQxOTo1Mjo0Ni4yMDI4NzU5MTYrMDAwMFx1MDAyNm1lcmNoYW50X2lkPTM0OHBrOWNnZjNiZ3l3MmJcdTAwMjZwdWJsaWNfa2V5PTJuMjQ3ZHY4OWJxOXZtcHIiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMzQ4cGs5Y2dmM2JneXcyYi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzM0OHBrOWNnZjNiZ3l3MmIvY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tLzM0OHBrOWNnZjNiZ3l3MmIifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiQWNtZSBXaWRnZXRzLCBMdGQuIChTYW5kYm94KSIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjp0cnVlLCJtZXJjaGFudEFjY291bnRJZCI6ImFjbWV3aWRnZXRzbHRkc2FuZGJveCIsImN1cnJlbmN5SXNvQ29kZSI6IlVTRCJ9LCJjb2luYmFzZUVuYWJsZWQiOmZhbHNlLCJtZXJjaGFudElkIjoiMzQ4cGs5Y2dmM2JneXcyYiIsInZlbm1vIjoib2ZmIn0="
-        let clientTokenURL = URL(string: "https://braintree-sample-merchant.herokuapp.com/client_token")!
+        let clientTokenURL = URL(string: "https://fastcart-braintree.herokuapp.com/client_token")!
         var clientTokenRequest = URLRequest(url: clientTokenURL)
         clientTokenRequest.setValue("text/plain", forHTTPHeaderField: "Accept")
         
@@ -48,12 +45,11 @@ class PaymentsViewController: UIViewController, BTDropInViewControllerDelegate {
             // As an example, you may wish to present our Drop-in UI at this point.
             // Continue to the next section to learn more...
             }.resume()
+        
         paymentStarted()
     }
 
     func paymentStarted() {
-        // ...
-        braintreeClient = BTAPIClient(authorization: CLIENT_AUTHORIZATION)
         // Create a BTDropInViewController
         let dropInViewController = BTDropInViewController(apiClient: braintreeClient!)
         dropInViewController.delegate = self
@@ -68,6 +64,7 @@ class PaymentsViewController: UIViewController, BTDropInViewControllerDelegate {
         let navigationController = UINavigationController(rootViewController: dropInViewController)
         present(navigationController, animated: true, completion: nil)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -83,8 +80,8 @@ class PaymentsViewController: UIViewController, BTDropInViewControllerDelegate {
     func postNonceToServer(paymentMethodNonce: String) {
         let fakePaymentMethodNonce = "fake-valid-nonce"
         print("posting to server")
-        let paymentURL = URL(string: "https://your-server.example.com/payment-methods")!
-        var request = NSMutableURLRequest(url: paymentURL)
+        let paymentURL = URL(string: "fastcart-braintree.herokuapp.com/client_token/checkout")!
+        let request = NSMutableURLRequest(url: paymentURL)
         request.httpBody = "payment_method_nonce=\(fakePaymentMethodNonce)".data(using: String.Encoding.utf8)
         request.httpMethod = "POST"
         
@@ -92,6 +89,7 @@ class PaymentsViewController: UIViewController, BTDropInViewControllerDelegate {
             // TODO: Handle success or failure
         }.resume()
     }
+    
     func drop(inViewControllerDidCancel viewController: BTDropInViewController) {
         dismiss(animated: true, completion: nil)
     }
