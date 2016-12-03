8c58889e898c4524392e94f5dca60e8ea6cb5181
Luis Perez <luis.perez.live@gmail.com>
2016-12-03 10:50:58 -0800

    add shybar

---
+++ FastCart/Pods/Target Support Files/TLYShyNavBar/TLYShyNavBar-umbrella.h
@@ -0,0 +1,11 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "TLYShyNavBar.h"
+#import "TLYShyNavBarFade.h"
+#import "TLYShyNavBarManager.h"
+
+FOUNDATION_EXPORT double TLYShyNavBarVersionNumber;
+FOUNDATION_EXPORT const unsigned char TLYShyNavBarVersionString[];
+
