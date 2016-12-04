32d9f86d9324182f673646a981d1888eabf26702
Luis Perez <luis.perez.live@gmail.com>
2016-12-04 12:43:59 -0800

    Improved scanner?

--- FastCart/FastCart/ScanViewController.swift
+++ FastCart/FastCart/ScanViewController.swift
@@ -12,38 +12,32 @@ import BarcodeScanner
 
 extension ScanViewController: BarcodeScannerCodeDelegate {
     func barcodeScanner(_ controller: BarcodeScannerController, didCaptureCode code: String, type: String) {
-        print(code)
         // perform network request
         WalmartClient.sharedInstance.getProductWithUPC(upc: code, success: { (products: [Product]) in
-            // save product and present correct view
+            guard products.count > 0 else {
+                Utilities.presentErrorAlert(title: "No Products", message: "Could not find product for code \(code).")
+                return
+            }
+            
+            // Present product modally.
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            
-            // try this
-            
             let productDetailsViewController = storyboard.instantiateViewController(withIdentifier: "ProductDetailsWithScrollViewController") as! ProductDetailsViewController
             
-//            let productDetailsNavigationController = storyboard.instantiateViewController(withIdentifier: "ProductDetailsNavigationController") as! UINavigationController
-//            let productDetailsViewController = productDetailsNavigationController.topViewController as! ProductDetailsViewController
             productDetailsViewController.product = products[0]
-            print("product\(products)")
-            self.navigationController?.pushViewController(productDetailsViewController, animated: true)
-//            self.present(productDetailsNavigationController, animated: true, completion: nil)
+            self.present(productDetailsViewController, animated: true, completion: {
+                controller.reset()
+            })
             
         }, failure: {(error: Error) -> () in
-            print(error.localizedDescription)
+            controller.resetWithError(message: "UPC: \(code) not found!")
         })
-
-        // dismiss after to test
-        controller.dismiss(animated: true, completion: nil)
-        // controller.reset()
-        
     }
 }
 
 extension ScanViewController: BarcodeScannerErrorDelegate {
     
     func barcodeScanner(_ controller: BarcodeScannerController, didReceiveError error: Error) {
-        print(error)
+        controller.resetWithError(message: error.localizedDescription)
     }
 }
 
@@ -57,78 +51,29 @@ extension ScanViewController: BarcodeScannerDismissalDelegate {
 class ScanViewController: UIViewController {
     var product: Product?
     
+    private var scanController: BarcodeScannerController!
     
     @IBAction func onFakeScanButtonPress(_ sender: Any) {
         let code = "787651241531"
-        WalmartClient.sharedInstance.getProductWithUPC(upc: code, success: { (products: [Product]) in
-            // save product and present correct view
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            
-            // try this
-            let productDetailsNavigationController = storyboard.instantiateViewController(withIdentifier: "ProductDetailsNavigationController") as! UINavigationController
-            let productDetailsViewController = productDetailsNavigationController.topViewController as! ProductDetailsViewController
-            productDetailsViewController.product = products[0]
-            print("product\(products)")
-            self.present(productDetailsNavigationController, animated: true, completion: nil)
-            
-        }, failure: {(error: Error) -> () in
-            print(error.localizedDescription)
-        })
-    
+        scanController.codeDelegate?.barcodeScanner(scanController, didCaptureCode: code, type: "upc")
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        print("loaded")
-        
-        
-    }
-    @IBOutlet weak var scanButton: UIButton!
-    
-    @IBAction func onScanButtonPress(_ sender: Any) {
-        // check to see if camera usage is authorized
-        if AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) ==  AVAuthorizationStatus.authorized
-        {
-            // Already Authorized
-            startScanning()
-        }
-        else
-        {
-            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { (granted :Bool) -> Void in
-                if granted == true
-                {
-                    // User granted
-                    self.startScanning()
-                }
-                else
-                {
-                    // User Rejected
-                    
-                }
-            });
-        }
-    }
-    
-    func startScanning() {
+   
+    // Creates the controller for scanning purposes.
+    private func createScanner() -> BarcodeScannerController {
         let controller = BarcodeScannerController()
         controller.codeDelegate = self
         controller.errorDelegate = self
         controller.dismissalDelegate = self
         
-        present(controller, animated: true, completion: nil)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-        
+        return controller
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewDidLoad() {
+        scanController = createScanner()
+        // If camera is available, push the scanner. Otherwise display default.
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            self.navigationController?.pushViewController(scanController, animated: true)
+        }
     }
-
-
 }
 
